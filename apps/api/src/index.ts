9babf78ee592566943fcb34bf1e22c425183746f
ankitku3101 <ankit.ku3101@gmail.com>
2025-06-20 15:29:59 +0530

    Implemented native websockets

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,13 +1,35 @@
-import { log } from 'console';
 import express, {Request, Response} from 'express'
+import { createServer } from 'http';
+import { WebSocketServer } from 'ws';
 
 const app = express();
-const PORT=3000;
+const server =  createServer();
 
-app.get('/', (req: Request, res: Response)=> {
-    res.send("Testing express");
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', (ws) => {
+    console.log("Web socket connection established");
+    
+    ws.on('message', (message) => {
+        console.log("Received: ", message.toString());
+        wss.clients.forEach(client => {
+            if (client.readyState === ws.OPEN) {
+                 client.send(`Echo: ${message}`);                
+            }
+        });
+    });
+
+    ws.on('close', () => {
+        console.log("Websocket connection closed");
+    })
+})
+
+app.get('/', (_, res) => {
+    res.send('Express + Websocket !')
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on https://localhost:${PORT}`);
-})
+const PORT = 5000;
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
+
