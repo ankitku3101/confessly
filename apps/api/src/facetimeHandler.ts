924dd6fe9d1e5498b5d258e613b07e52222c5913
ankitku3101 <ankit.ku3101@gmail.com>
2025-07-02 06:54:15 +0530

    feat(api): add signalling server code

--- apps/api/src/facetimeHandler.ts
+++ apps/api/src/facetimeHandler.ts
@@ -4,6 +4,7 @@ let waitingUser: Socket | null = null;
 export function facetimeHandler(io: Server, socket: Socket) {
     console.log("facetimeHandler attached:", socket.id);
 
+    //This part of code lets the users find partners 
     socket.on("find_partner", () => {
         if (waitingUser === null) {
             waitingUser = socket;
@@ -21,5 +22,19 @@ export function facetimeHandler(io: Server, socket: Socket) {
             console.log("Waiting User disconnected: ", socket.id);
             waitingUser = null;
         }
+    })
+    // ...till here
+
+    // This part is pure WebRTC stuff for sending ICE candidates and SDP.
+    socket.on("offer", ({target, sdp}) => {
+        io.to(target).emit('offer', { sdp, sender: socket.id })
+    })
+
+    socket.on("answer", ({target, sdp}) => {
+        io.to(target).emit('answer', { sdp, sender: socket.id })
+    })
+
+    socket.on("ice-candidate", ({target, candidate}) => {
+        io.to(target).emit('answer', { candidate, sender: socket.id })
     })
 }
