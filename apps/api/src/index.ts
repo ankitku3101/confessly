e58c173da7bbe436b53802ac4fff6c77b97f043a
ankitku3101 <ankit.ku3101@gmail.com>
2025-07-09 12:21:19 +0530

    build: remove facetime refs

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
-import { facetimeHandler } from './facetimeHandler';
 
 const app = express();
 const httpServer = createServer(app);
