31b53ae05047a5ebbef2ab11aca441e57e98fb25
ankitku3101 <ankit.ku3101@gmail.com>
2025-07-09 19:04:26 +0530

    feat(web): change font to Roboto

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,17 +1,18 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 import { MagicCursor } from "@/components/MagicCursor";
-import { StarsBackground } from "@/components/ui/stars-background"; // 👈 import this
+import { StarsBackground } from "@/components/ui/stars-background"; 
 import { ShootingStars } from "@/components/ui/shooting-stars";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+
+const robotoSans = Roboto({
+  variable: "--font-roboto-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const robotoMono = Roboto_Mono({
+  variable: "--font-roboto-mono",
   subsets: ["latin"],
 });
 
@@ -28,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased relative bg-black text-white overflow-hidden`}
+        className={`${robotoSans.variable} ${robotoMono.variable} antialiased relative bg-black text-white overflow-hidden`}
       >
         <StarsBackground className="absolute inset-0 -z-10 pointer-events-none" />
         <ShootingStars className="absolute inset-0 -z-10 pointer-events-none" />
